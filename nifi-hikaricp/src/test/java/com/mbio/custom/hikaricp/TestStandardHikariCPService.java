a5a79efd20b55aca1750a3b725e1caffcf10682e
mbio <qnwlqnwlxm@naver.com>
2019-10-29 04:10:44 +0900

    HikariCPControllerSerivce-3

--- nifi-hikaricp/src/test/java/com/mbio/custom/hikaricp/TestStandardHikariCPService.java
+++ nifi-hikaricp/src/test/java/com/mbio/custom/hikaricp/TestStandardHikariCPService.java
@@ -16,28 +16,55 @@
  */
 package com.mbio.custom.hikaricp;
 
+import java.io.PrintWriter;
+import org.apache.derby.drda.NetworkServerControl;
+import org.apache.derby.jdbc.ClientDataSource;
 import org.apache.nifi.reporting.InitializationException;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestStandardHikariCPService {
-
-    @Before
-    public void init() {
-
+  
+  private static NetworkServerControl derby;
+  
+    @BeforeClass
+    public static void init() throws Exception {
+      derby = new NetworkServerControl();
+      derby.start(new PrintWriter(System.out));
     }
-
+    
+    @AfterClass
+    public static void stop() throws Exception {
+      derby.shutdown();
+    }
+    
     @Test
     public void testService() throws InitializationException {
         final TestRunner runner = TestRunners.newTestRunner(TestProcessor.class);
         final StandardHikariCPService service = new StandardHikariCPService();
-        runner.addControllerService("test-good", service);
-
+        runner.addControllerService("dbcp", service);
+        
+        runner.setProperty(service, ConfigUtil.DATASOURCE_CLASSNAME, ClientDataSource.class.getName());
+        runner.setProperty(service, ConfigUtil.USERNAME, "test");
+        runner.setProperty(service, ConfigUtil.PASSWORD, "test");
+        runner.setProperty(service, ConfigUtil.AUTO_COMMIT, "true");
+        runner.setProperty(service, ConfigUtil.METRICS, "true");
+        
+        runner.setProperty(service, "databaseName", "test");
+        runner.setProperty(service, "createDatabase", "create");
+        runner.setProperty(service, "serverName", "localhost");
+        runner.setProperty(service, "portNumber", "1527");
         runner.enableControllerService(service);
+        runner.setProperty(TestProcessor.DS_PROP, "dbcp");
+        
+        runner.run();
 
         runner.assertValid(service);
+        runner.disableControllerService(service);
+        runner.shutdown();
     }
 
 }
